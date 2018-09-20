558035f19ee2618a2bccaad9acff94550a66b0d5
ngalang92 <nickgalangsf@gmail.com>
2018-09-20 02:33:19 -0700

    checkpoint_3

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,17 +5,15 @@ const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
-//#1
+
   describe("GET /", () => {
 
-//#2
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
 
-//#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Bloccit");
 
-//#4
         done();
       });
     });
