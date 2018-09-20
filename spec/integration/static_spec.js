a15da37eff0ddd956df6d079941c37b20bdb9f44
ngalang92 <nickgalangsf@gmail.com>
2018-09-19 19:10:03 -0700

    assignment_1

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
@@ -19,5 +20,16 @@ describe("routes : static", () => {
       });
     });
 
-  });
-});
+  }); //end describe GET
+
+  describe("GET /marco", () => {
+		it("should return status code 200 and have 'polo' in the body of the response", done => {
+			request.get(marco, (err, res, body) => {
+				expect(res.statusCode).toBe(200);
+				expect(body).toBe('polo');
+				done();
+			});
+		});
+	}); // end describe GET/marco
+
+}); //end describe routes:static
---
+++ src/controllers/staticController.js
@@ -0,0 +1,10 @@
+module.exports = {
+	index(req, res, next) {
+		res.render('static/index', { title: 'Welcome to Bloccit' });
+	},
+
+	marco(req, res, next) {
+		res.render('static/marco', { body: 'polo' });
+	}
+
+};
