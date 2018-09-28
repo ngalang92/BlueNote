4e584aabe54d0b9c1e2cb5845cadb2163d88dbdf
ngalang92 <nickgalangsf@gmail.com>
2018-09-27 21:46:38 -0700

    assignment_8

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -81,8 +81,35 @@ describe("routes : topics", () => {
           });
         }
       );
-    });
+    }); //end it
+
+    it("should not create a new topic that fails validations", (done) => {
+     const options = {
+       url: `${base}/${this.topic.id}/create`,
+       form: {
+         title: "a",
+         description: "b"
+       }
+     };
+      request.post(options,
+       (err, res, body) => {
+         Topic.findOne({where: {title: "a"}})
+         .then((topic) => {
+             expect(topic).toBeNull();
+             done();
+         })
+         .catch((err) => {
+           console.log(err);
+           done();
+         });
+       }
+     );
+   }); //end it validation 
+
+
   }); // end describe POST topics create
+
+
 
   describe("GET /topics/:id", () => {
 
